b13268017a2f5c1f6ae9b9b3bb4dbd57e7a7bed7
lesha_grishchenko_04 <grishchenkoalexey2004@yandex.ru>
2022-11-28 00:33:36 +0300

    add: connected main file with modules

--- Task5/shell.c
+++ Task5/shell.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include "lexem_list_types.h"
+#include "syntax_tree_types.h"
 
 list_type get_lexem_list(status *program_status);
 
+void print_string_list(list_type lst);
+
+int get_lexem_list_len(list_type lst);
+
+cmd_link build_syntax_tree(list_type lst,int start_index,int end_index);
 
 int main(int argc,char *argv[]){
 	status program_status;
-	list_type lexem_list = NULL;
+ 	int lexem_list_len = 0;	
+ 	list_type lexem_list = NULL;
+	program_status = Success;
+	cmd_link tree_root = NULL;
 
-	while (program_status == Success){
+	while(1){
 		printf("=>");
 		lexem_list = get_lexem_list(&program_status);
 
+		if (program_status == Success){
+			//print_string_list(lexem_list);
+			lexem_list_len = get_lexem_list_len(lexem_list);
+			if (lexem_list_len)
+				tree_root = build_syntax_tree(lexem_list,0,lexem_list_len-1);
+		}
+
+		else if (program_status == Finish){
+			print_string_list(lexem_list);
+			break;
+		}
+		else 
+			break;
+
 	}
+	
+	
+		 
+	
 
 	if (program_status == Error){
 		return 1;
